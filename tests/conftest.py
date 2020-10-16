7be6b21859325fe0545e46ce719f76a07c5cb4bd
Tobias Persson <tobiaspn@axis.com>
2020-10-16 12:39:55 +0200

    Fix problem with tests failing sometimes (#56)

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,10 +17,18 @@
 """Pytest configuration."""
 import os
 import threading
+import time
+import logging
 import pytest
+from requests.exceptions import HTTPError
+from tests.lib.query_handler import GraphQLQueryHandler
 from eiffel_graphql_api.graphql.api import APP
 from eiffel_graphql_api.graphql.db.database import get_database
 from eiffel_graphql_api.graphql.db.database import get_client
+
+
+logging.basicConfig(level=logging.DEBUG)
+LOGGER = logging.getLogger(__name__)
 
 
 @pytest.fixture
@@ -39,6 +47,26 @@ def start():
     APP.run("127.0.0.1", 12345)
 
 
+def wait_for_webserver_connection():
+    """Wait for the webserver to respond to HTTP requests."""
+    LOGGER.info("Testing for webserver connectivity.")
+    query_handler = GraphQLQueryHandler("http://127.0.0.1:12345/graphql")
+    timeout = time.time() + 30
+    while time.time() < timeout:
+        try:
+            query_handler.execute("{nothing}")
+            LOGGER.info("Up and running")
+            return
+        except HTTPError:  # BadRequest means the webserver came up.
+            LOGGER.info("Up and running")
+            return
+        except Exception as exception:  # pylint: disable=broad-except
+            LOGGER.error("%r", exception)
+        LOGGER.warning("Connection could not be established. Trying again in 1s..")
+        time.sleep(1)
+    raise TimeoutError("Timeout waiting for the webserver to start.")
+
+
 @pytest.fixture(scope="session", autouse=True)
 def start_server(
     request,
@@ -48,6 +76,7 @@ def start_server(
     thread = threading.Thread(target=start)
     thread.daemon = True
     thread.start()
+    wait_for_webserver_connection()
     client.drop_database(os.getenv("MONGODB_DATABASE"))
 
     def start_server_fin():  # pylint:disable=unused-variable
